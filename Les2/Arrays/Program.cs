06b5697469fd89e380fbbd52d2bf177986adca1a
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2022-02-15 17:37:30 +0100

    Les 2 helemaal afgemaakt van Trello

---
+++ Les2/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 10, 20, 30, 40, 50, 60 };
+
+            Console.WriteLine(myArray[0]);  
+            Console.WriteLine(myArray[1]);  
+            Console.WriteLine(myArray[2]);  
+            Console.WriteLine(myArray[3]);  
+            Console.WriteLine(myArray[4]);  
+            Console.WriteLine(myArray[5]);  
+        }
+    }
+}
---
+++ Les2/Variables en Datatypes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Les2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = 0;
+            float komma = 0.5f;
+            bool ground = true;  
+        }
+    }
+}
